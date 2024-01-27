73897ad4aa1c79d10e99fd728af4735933ae6158
Frank <frank.reimann@jambit.de>
2024-01-27 03:29:23 +0100

    "jeeves: updating documentation for ./test/test.ts"

--- tools/code-jeeves/test/test.ts
+++ tools/code-jeeves/test/test.ts
@@ -1,12 +1,9 @@
-// !
 import fs from "fs";
 
-// !
 function isEven(num: number): boolean {
   return num % 2 === 0;
 }
 
 export function addNums(a: number, b: number): number {
-  // sum of a and b
-  return a+ a - a + b + b -b;
+  return a + b;
 }
