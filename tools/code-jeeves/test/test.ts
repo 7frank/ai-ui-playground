1fa419cca0e38799ee40096cd92023293aabd704
Frank <frank.reimann@jambit.de>
2024-01-27 02:56:49 +0100

    add test file

---
+++ tools/code-jeeves/test/test.ts
@@ -0,0 +1,12 @@
+// !
+import fs from "fs";
+
+// !
+function isEven(num: number): boolean {
+  return num % 2 === 0;
+}
+
+export function addNums(a: number, b: number): number {
+  // sum of a and b
+  return a + b;
+}
