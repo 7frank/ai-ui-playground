d75e9abc8a3b38eb6b6c1154aa99e8cb9283f4ee
Frank <frank.reimann@jambit.de>
2024-02-04 22:05:43 +0100

    update schema for serialization

--- tools/code-jeeves/src/types/taskFileSchema.ts
+++ tools/code-jeeves/src/types/taskFileSchema.ts
@@ -1,38 +1,58 @@
 import { z } from "zod";
 
+const functionName = z
+  .string()
+  .describe(
+    "function name in camelCase notation. The same function name like in the function declaration"
+  );
+const task = z.string().describe("What is the task to be done for this step?");
+
+const ext = z
+  .string()
+  .optional()
+  .describe(
+    "The file extension from which to infer the source code language."
+  );
+const declaration = z.string().describe("the function declaration");
+const preferences = z
+  .string()
+  .optional()
+  .describe(
+    "do you have suggestions or constraints? e.g. use a specific library?"
+  );
 /**
  * Section specifies the form the response of generating a `plan` via openai has to have.
+ * 
+ * Note: we defined 3 levels of tasks > subtasks > subsubtasks 
+ *  So that serialization to json is not an issue.
+ *  Which it was previously with recursive definition.
  */
 
 const baseTaskSchema = z.object({
-  functionName: z
-    .string()
-    .describe(
-      "function name in camelCase notation. The same function name like in the function declaration",
-    ),
-  task: z.string().describe("What is the task to be done for this step?"),
-  ext: z
-    .string()
-    .optional()
-    .describe(
-      "The file extension from which to infer the source code language.",
-    ),
-  declaration: z.string().describe("the function declaration"),
-  preferences: z
-    .string()
-    .optional()
-    .describe(
-      "do you have suggestions or constraints? e.g. use a specific library?",
-    ),
+  functionName,
+  task,
+  ext,
+  declaration,
+  preferences,
 });
 
-export type TaskSchema = z.infer<typeof baseTaskSchema> & {
+
+ type SubTaskSchema = z.infer<typeof baseTaskSchema> & {
+  subTasks?: SubTaskSchema[];
+};
+
+ const SubTaskSchema: z.ZodType<SubTaskSchema> = baseTaskSchema.extend({
+  subTasks: baseTaskSchema.array()
+    .optional()
+    .describe("A list of subtasks of the parent task."),
+});
+
+export type TaskSchema = z.infer<typeof SubTaskSchema> & {
   subTasks?: TaskSchema[];
 };
 
 export const TaskSchema: z.ZodType<TaskSchema> = baseTaskSchema.extend({
-  subTasks: z
-    .lazy(() => TaskSchema.array())
+  subTasks: SubTaskSchema.array()
     .optional()
     .describe("A list of subtasks of the parent task."),
 });
