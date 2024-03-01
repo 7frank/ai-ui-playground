2af798837b358ca500669ff4988648a7b0e862e9
Frank <frank.reimann@jambit.de>
2024-03-01 22:17:54 +0100

    adapter json and result

---
+++ tools/huggingface-k8s-dockerizer/adapter/infoEndpoint.py
@@ -0,0 +1,84 @@
+// api_name: /predict
+
+from gradio_client import Client
+
+client = Client("https://diffusers-unofficial-sdxl-turbo-i2i-t2i.hf.space/--replicas/4z4rb/")
+result = client.predict(
+		https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png,	# filepath  in 'Webcam' Image component
+		"Hello!!",	# str  in 'parameter_5' Textbox component
+		0,	# float (numeric value between 0.0 and 1.0) in 'Strength' Slider component
+		1,	# float (numeric value between 1 and 10) in 'Steps' Slider component
+		0,	# float (numeric value between 0 and 12013012031030) in 'Seed' Slider component
+							api_name="/predict"
+)
+print(result)
+
+
+// Return Type(s)
+// # filepath representing output in 'value_11' Image component
+
+
+#############
+
+
+// api_name: /predict_1
+
+from gradio_client import Client
+
+client = Client("https://diffusers-unofficial-sdxl-turbo-i2i-t2i.hf.space/--replicas/4z4rb/")
+result = client.predict(
+		https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png,	# filepath  in 'Webcam' Image component
+		"Hello!!",	# str  in 'parameter_5' Textbox component
+		0,	# float (numeric value between 0.0 and 1.0) in 'Strength' Slider component
+		1,	# float (numeric value between 1 and 10) in 'Steps' Slider component
+		0,	# float (numeric value between 0 and 12013012031030) in 'Seed' Slider component
+							api_name="/predict_1"
+)
+print(result)
+
+
+// Return Type(s)
+// # filepath representing output in 'value_11' Image component
+
+
+##############
+
+// api_name: /predict_2
+
+from gradio_client import Client
+
+client = Client("https://diffusers-unofficial-sdxl-turbo-i2i-t2i.hf.space/--replicas/4z4rb/")
+result = client.predict(
+		https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png,	# filepath  in 'Webcam' Image component
+		"Hello!!",	# str  in 'parameter_5' Textbox component
+		0,	# float (numeric value between 0.0 and 1.0) in 'Strength' Slider component
+		1,	# float (numeric value between 1 and 10) in 'Steps' Slider component
+		0,	# float (numeric value between 0 and 12013012031030) in 'Seed' Slider component
+							api_name="/predict_2"
+)
+print(result)
+
+//  Return Type(s)
+// # filepath representing output in 'value_11' Image component
+
+
+###################
+
+// api_name: /predict_3
+
+from gradio_client import Client
+
+client = Client("https://diffusers-unofficial-sdxl-turbo-i2i-t2i.hf.space/--replicas/4z4rb/")
+result = client.predict(
+		https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png,	# filepath  in 'Webcam' Image component
+		"Hello!!",	# str  in 'parameter_5' Textbox component
+		0,	# float (numeric value between 0.0 and 1.0) in 'Strength' Slider component
+		1,	# float (numeric value between 1 and 10) in 'Steps' Slider component
+		0,	# float (numeric value between 0 and 12013012031030) in 'Seed' Slider component
+							api_name="/predict_3"
+)
+print(result)
+
+ Return Type(s)
+# filepath representing output in 'value_11' Image component
+
